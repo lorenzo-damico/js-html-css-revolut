a4efe76291f0e23e08763b2be6012902f0b1b85b
lorenzo-damico <loredam@hotmail.it>
2020-09-04 10:45:22 +0200

    Correzioni varie

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
 
-  $(".open_menu").mouseenter(
-    function() {
-      $(this).children(".menu_tendina").show();
-    }
-  );
-
-  $(".open_menu").mouseleave(
-    function() {
-      $(this).children(".menu_tendina").hide();
+  $(".open_menu").hover(
+    function () {
+      $(this).children(".menu_tendina").toggle();
     }
   );
 
 
+  // SOLUZIONE CON 2 FUNZIONI
 
-
-
-
+  // $(".open_menu").mouseenter(
+  //   function() {
+  //     $(this).children(".menu_tendina").show();
+  //   }
+  // );
+  //
+  // $(".open_menu").mouseleave(
+  //   function() {
+  //     $(this).children(".menu_tendina").hide();
+  //   }
+  // );
 
 });
